bdc2a053bdb75d9800e1a163dc532f551fa2f3ba
Roma Lososev <korsunov.rom@yandex.ru>
2024-04-12 10:11:12 +0300

    Write script in l2.js

--- l2.js
+++ l2.js
@@ -1,5 +1,5 @@
-let a = "Я изучаю JavaScript"
-console.log("\""+a +"\" - это исходный текст")
-console.log(a.charAt(2) + " это 3й символ")
-console.log(a.charAt(0) + " это первый символ")
-console.log(a.charAt(a.length-1) + " это последний символ")
+let a = "Я изучаю JavaScipt"
+console.log("\""+a+"\" - исходный текст")
+console.log("\""+a.replace("ю","л")+"\" - текст с изменённым символом ю")
+console.log("\""+a.replace("JavaScript","Frontend")+"\" - текст, где заменено слово JavaScript на Frontend")//ничего не поменяется, т.к в исходном тексте написано JavaScipt, а не JavaScript
+console.log("\""+a.replace("JavaScipt","Frontend")+"\" - текст, где заменено слово JavaScipt на Frontend")

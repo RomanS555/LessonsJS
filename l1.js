4b536657a1360e5dd1070582803393fd27a28ac7
Roma Lososev <korsunov.rom@yandex.ru>
2024-04-12 09:56:45 +0300

    Added comments for l1.js and created file l2.js

--- l1.js
+++ l1.js
@@ -1,2 +1,6 @@
-let a = "ABC"
-console.log(a.charAt(2))
+let a = "Я изучаю JavaScript"
+console.log("\""+a +"\" - это исходный текст")
+console.log(a.charAt(2) + " это 3й символ")
+console.log(a.charAt(0) + " это первый символ")
+console.log(a.charAt(a.length-1) + " это последний символ ")//универсальный способ
+console.log(a.charAt(18) + " это последний символ ")//конкретно для этого текста
---
+++ l2.js
@@ -0,0 +1,5 @@
+let a = "Я изучаю JavaScript"
+console.log("\""+a +"\" - это исходный текст")
+console.log(a.charAt(2) + " это 3й символ")
+console.log(a.charAt(0) + " это первый символ")
+console.log(a.charAt(a.length-1) + " это последний символ")

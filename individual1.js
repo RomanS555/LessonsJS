c553cc32a6cb100ebbb88643d5f1dc899da895aa
Roma Lososev <korsunov.rom@yandex.ru>
2024-04-16 11:54:04 +0300

    edit script individual1.js

--- individual1.js
+++ individual1.js
@@ -2,6 +2,8 @@ let a = prompt()
 let an = Number(a)
 let twovalues = [0,1]
 let v = 0
+if(an > 0 && an == Math.round(an)){
+
 
 for(let i = 1; i < an; i++){
   v = twovalues[0] + twovalues[1]
@@ -11,5 +13,8 @@ for(let i = 1; i < an; i++){
       twovalues[1] = v
     }
     console.log(v)
+}alert(v)
+}else{
+  alert("КРИТИЧЕСКАЯ ОШИБКА! ВСЁ СЛОМАЛОСЬ И ЕСТЬ ПОВЫШЕННЫЙ РИСК ТОГО ЧТО ВСЁ СЛОМАЕТСЯ ЕЩЁ РАЗ НЕ НУЖНО ТАК ДЕЛАТЬ")
+  console.error("КРИТИЧЕСКАЯ ОШИБКА! ВСЁ СЛОМАЛОСЬ И ЕСТЬ ПОВЫШЕННЫЙ РИСК ТОГО ЧТО ВСЁ СЛОМАЕТСЯ ЕЩЁ РАЗ НЕ НУЖНО ТАК ДЕЛАТЬ")  
 }
-alert(v)
